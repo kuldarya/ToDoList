af3d044b28670b9be803754fc717f8d848cf2fea
Daria.Kuleshova <daria.kuleshova@life.com.by>
2019-08-26 18:06:57 +0300

    Initial Commit

---
+++ ToDoListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Darya Kuliashova on 8/26/19.
+//  Copyright © 2019 Darya Kuliashova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
