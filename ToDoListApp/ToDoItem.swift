db0ed3a29bbeebedfe49ba67f3553f8a0fd043e9
Darya Kuliashova <kuliashova.darya@gmail.com>
2019-09-16 17:19:17 +0300

    First commit

--- ToDoListApp/ToDoItem.swift
+++ ToDoListApp/ToDoItem.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+class ToDoItem {
+    var title: String
+    var isCompleted: Bool
+    
+    init(title: String) {
+        self.title = title
+        self.isCompleted = false
+    }
+    
+    public class func getDefaultData() -> [ToDoItem] {
+        return [
+                ToDoItem(title: "Buy milk"),
+                ToDoItem(title: "Clean up the flat"),
+                ToDoItem(title: "Call a mum"),
+                ToDoItem(title: "Create a todo list app")
+        ]
+    }
+}
--- ToDoListApp/ViewController.swift
+++ ToDoListApp/ViewController.swift
@@ -8,13 +8,107 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var items = ToDoItem.getDefaultData()
+    var selectedRowForEditing: IndexPath?
+    var selectedRowEditingSuccess: ((Bool) -> Void)?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setup()
     }
-
-
+    
+    private func setup() {
+        self.title = "To Do List"
+        createAddButton()
+    }
+    
+    private func createAddButton() {
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapOnAddButton))
+    }
+    
+    @objc func didTapOnAddButton() {
+        showAddNewItemAlert(title: "Add a new todo item", message: "Enter new todo")
+    }
+    
+    private func showAddNewItemAlert(title: String, message: String) {
+        let addDialog = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        addDialog.addTextField(configurationHandler: nil)
+        addDialog.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        addDialog.addAction(UIAlertAction(title: "Add", style: .default, handler: { action in
+            let title = addDialog.textFields?[0].text?.capitalized
+            if let title = title {
+                let newItem = ToDoItem(title: title)
+                self.items.append(newItem)
+                
+                let path = IndexPath(row: self.items.count - 1, section: 0)
+                self.tableView.insertRows(at: [path], with: .fade)
+            }
+        }))
+        self.present(addDialog, animated: true)
+    }
+    
+    private func showEditItemAlert(indexPath: IndexPath) {
+        let editDialog = UIAlertController(title: "Edit", message: "Edit your todo", preferredStyle: .alert)
+        editDialog.addTextField { _ in
+            editDialog.textFields?[0].text = self.items[indexPath.row].title
+        }
+        editDialog.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        editDialog.addAction(UIAlertAction(title: "Save", style: .default, handler: { action in
+            let title = editDialog.textFields?[0].text
+            if let title = title,
+                let row = self.selectedRowForEditing?.row,
+                let indexPath = self.selectedRowForEditing {
+                self.items[row].title = title
+                self.selectedRowEditingSuccess?(true)
+                self.tableView.reloadRows(at: [indexPath], with: .fade)
+            }
+        }))
+        self.present(editDialog, animated: true)
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todo_cell", for: indexPath)
+        
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+        let accessoryType: UITableViewCell.AccessoryType = item.isCompleted ? .checkmark : .none
+        cell.accessoryType = accessoryType
+        
+        return cell
+    }
+    
+    //MARK: - UITableViewDelegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let item = items[indexPath.row]
+        item.isCompleted = !item.isCompleted
+        tableView.reloadRows(at: [indexPath], with: .fade)
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        items.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .fade)
+    }
+    
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let action = UIContextualAction(style: .normal, title: "Edit") { (action, view, success: @escaping (Bool) -> Void) in
+            self.selectedRowForEditing = indexPath
+            self.selectedRowEditingSuccess = success
+            
+            self.showEditItemAlert(indexPath: indexPath)
+        }
+        return UISwipeActionsConfiguration(actions: [action])
+    }
 }
 
